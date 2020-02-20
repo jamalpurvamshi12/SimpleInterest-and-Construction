6a2504a0d8b66bdde7832e16b02d7453fd69ed9e
jamalpurvamshi12 <60353700+jamalpurvamshi12@users.noreply.github.com>
2020-02-21 00:21:40 +0530

    Add files via upload

---
+++ MainTestConstructionCost.java
@@ -0,0 +1,12 @@
+package com.sici;
+
+public class MainTestConstructionCost {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		ConstructionCost cc=new ConstructionCost();
+
+	}
+
+}
